08ee0da4e98798b830049c03b5c875c4da9330ee
CryptoChloe <cryptogames.chloe@gmail.com>
2025-07-05 13:00:04 +1000

    added more items to the selection

--- src/items.js
+++ src/items.js
@@ -13,6 +13,15 @@ const items = [
     description: "A basic iron sword."
   },
   {
+    id: "sword_002",
+    name: "Mana Sword",
+    type: "weapon",
+    rarity: "common",
+    properties: { attack: 7 },
+    icon: "../assets/sprites/sword2.png",
+    description: "A basic mana sword."
+  },
+  {
     id: "armor_001",
     name: "Leather Armor",
     type: "body",
@@ -20,6 +29,14 @@ const items = [
     properties: { defense: 3 },
     icon: "../assets/sprites/armor.png",
     description: "Simple leather armor."
+  },{
+    id: "armor_002",
+    name: "Iron Armor",
+    type: "body",
+    rarity: "common",
+    properties: { defense: 8 },
+    icon: "../assets/sprites/armor2.png",
+    description: "Simple Iron armor."
   },
   {
     id: "amulet_001",
@@ -29,6 +46,14 @@ const items = [
     properties: { luck: 2 },
     icon: "../assets/sprites/amulet.png",
     description: "Increases your luck."
+  },{
+    id: "amulet_002",
+    name: "Amulet of Restoration",
+    type: "amulet",
+    rarity: "rare",
+    properties: { health: 20 },
+    icon: "../assets/sprites/amulet2.png",
+    description: "Increases your Health."
   },
   {
     id: "potion_hp_small",
@@ -36,8 +61,16 @@ const items = [
     type: "consumable",
     rarity: "common",
     properties: { heal: 50 },
-    icon: "../assets/sprites/potion.png",
+    icon: "../assets/sprites/potion2.png",
     description: "Restores 50 HP."
+  },{
+    id: "potion_hp_mana",
+    name: "Small Mana Potion",
+    type: "consumable",
+    rarity: "common",
+    properties: { Mana: 50 },
+    icon: "../assets/sprites/potion.png",
+    description: "Restores 50 Mana."
   },
   // Add more items as needed
 ];
